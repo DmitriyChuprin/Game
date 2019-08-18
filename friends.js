902f2bd806be15e8795954e9a3ff18aa6583a516
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-08-18 23:55:53 +0300

    add

--- friends.js
+++ friends.js
@@ -1,3 +1,3 @@
-VK.api("friends.search", { "fields": "photo_100", "count": 1000 }, function(data) {
+VK.api("friends.search", { "fields": "photo_100", "count": "1000" }, function(data) {
     console.log(data);
 });
