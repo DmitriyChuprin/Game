72e281c2aaef089e581e2c2aa98a625cc6efe384
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-08-19 23:18:07 +0300

    add

--- scripts/vk.js
+++ scripts/vk.js
@@ -1,26 +1,28 @@
-import allfriends from "./js/friends.js";
+import { allfriends } from "./friends.js";
+
 function next(lst) {
-    let tempMassive = lst.slice(count, count+8);
+    let tempMassive = lst.slice(count, count + 8);
     console.log("Length: " + tempMassive.length);
     tempMassive.forEach(function(item) {
         console.log(item);
     });
     count += tempMassive.length;
     if (count === 21) {
-      count = 20;
+        count = 20;
     };
 };
 
 function previous(lst) {
-  let newCount = count - 8;
+    let newCount = count - 8;
     if (newCount < 0) {
-      newCount = 0;}
+        newCount = 0;
+    }
     let tempMassive = lst.slice(newCount, count);
     console.log("Length: " + tempMassive.length);
     tempMassive.forEach(function(item) {
         console.log(item);
     });
-    count -= tempMassive.length;    
+    count -= tempMassive.length;
 }
 
 next(allfriends);
