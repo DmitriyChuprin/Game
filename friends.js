713e957022fdcde8540347cc5d1e40d3f7481c76
Dmitriy <chuprin-dmitriy@mail.ru>
2019-08-19 16:07:20 +0300

    change js

--- friends.js
+++ friends.js
@@ -1,3 +1,3 @@
 export let allfriends = VK.api("friends.search", { "fields": "photo_100", "count": "1000" }, function(data) {
-    console.log(data.response);
+    console.log(data.response.items);
 });
