c9c4a51b30b7e180752aba351ac7c55ffffa449d
Dmitriy <chuprin-dmitriy@mail.ru>
2019-08-20 16:18:41 +0300

    add

--- scripts/friends.js
+++ scripts/friends.js
@@ -1,6 +1,7 @@
 let allfriends = [];
 VK.api("friends.get", { "fields": "photo_100", "count": "1000" }, function(data) {
     let friends = data.response.length;
+    console.log(friends);
     for (let i=0; i < friends; i++) {
         allfriends.push(data.response[i]);
     }
