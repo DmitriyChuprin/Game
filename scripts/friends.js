9d6002a53dfd695d14a3135e89923d4a34e8b03d
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-08-21 20:57:51 +0300

    w

--- scripts/friends.js
+++ scripts/friends.js
@@ -4,5 +4,5 @@ VK.api("friends.get", { "fields": "photo_100", "count": "1000" }, function(data)
         allfriends.push(element);
     });
 });
-
+console.log(allfriends);
 //export { allfriends };
