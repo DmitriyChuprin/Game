47311b628252cddb7eb72a09a57513cb5691f546
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-08-22 11:23:42 +0300

    ыв

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,4 @@
-VK.api("friends.get", { "fields": "photo_100", "order": "name", "count": "8", "offset": "8" }, function(data) {
+VK.api("friends.get", { "fields": "photo_100", "order": "name", "count": "8", "offset": "0" }, function(data) {
     //let friends = data.response.items;
     console.log(data);
 });
