e7f45a275be3040590a9d3489f4f4ffca199ada0
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-08-22 11:17:57 +0300

    ad

--- scripts/test.js
+++ scripts/test.js
@@ -1,77 +1,9 @@
-let test = {
-    "response": {
-        "count": 1132,
-        "items": [{
-            "id": 2884043,
-            "first_name": "Albert",
-            "last_name": "Usmanov",
-            "domain": "hghg",
-            "city": {
-                "id": 1,
-                "title": "Moscow"
-            },
-            "online": 0
-        }, {
-            "id": 1894768,
-            "first_name": "Aleksandra",
-            "last_name": "Sokolovskaya",
-            "domain": "aleksandrasokolovskaya",
-            "city": {
-                "id": 1,
-                "title": "Moscow"
-            },
-            "online": 0
-        }, {
-            "id": 158157120,
-            "first_name": "Alenka",
-            "last_name": "Scherbakova",
-            "domain": "id158157120",
-            "city": {
-                "id": 2,
-                "title": "Saint Petersburg"
-            },
-            "online": 1
-        }]
-    }
-}
+VK.api("friends.get", { "fields": "photo_100", "order": "name", "count": "8", "offset": "8" }, function(data) {
+    //let friends = data.response.items;
+    console.log(data);
+});
 
-function getFriends() {
-    let allfriends = [];
-    let friends = test.response.items;
-    friends.forEach(element => {
-        allfriends.push(element);
-    })
-    return allfriends;
-}
-
-let friends = getFriends();
-let count = 0;
-console.log("ARRAY FRIENDS: " + friends);
-
-function next(lst) {
-    console.log("FIRST ITEM: " + lst[0]);
-    let tempMassive = lst.slice(count, count + 8);
-    console.log("Length: " + tempMassive.length);
-    tempMassive.forEach(function(item) {
-        console.log(item);
-    });
-    count += tempMassive.length;
-    if (count === 21) {
-        count = 20;
-    };
-};
-
-function previous(lst) {
-    let newCount = count - 8;
-    if (newCount < 0) {
-        newCount = 0;
-    }
-    let tempMassive = lst.slice(newCount, count);
-    console.log("Length: " + tempMassive.length);
-    tempMassive.forEach(function(item) {
-        console.log(item);
-    });
-    count -= tempMassive.length;
-}
-
-next(friends);
+VK.api("friends.get", { "fields": "photo_100", "order": "name", "count": "8", "offset": "8" }, function(data) {
+    //let friends = data.response.items;
+    console.log(data);
+});
