ee202d945cae3d9f46953457ec0728d10dc26122
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-08-23 23:37:59 +0300

    df

--- scripts/test.js
+++ scripts/test.js
@@ -2,6 +2,7 @@ function next() {
     let click = 8;
     VK.api("friends.get", { "fields": "photo_100", "order": "name", "count": "8", "offset": click }, function(data) {
         //let friends = data.response.items;
+        console.log(`Length = ${data.response.items.length}`);
         data.response.items.forEach(element => {
             console.log(`ID: ${element.id} name ${element.first_name}`);
         });
