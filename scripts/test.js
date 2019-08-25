ebdd5a5c4b122fa727fe893bc3af0da82896c11b
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-08-25 09:01:28 +0300

    df

--- scripts/test.js
+++ scripts/test.js
@@ -4,7 +4,7 @@ function next() {
         //let friends = data.response.items;
         console.log(`Length = ${data.response.items.length}`);
         data.response.items.forEach(element => {
-            console.log(`ID: ${element.id} name ${element.first_name}, `);
+            console.log(`ID: ${element.id} name ${element.first_name}, url: ${element.photo_50}, onlline: ${element.online}`);
         });
         click += 8;
     });
