02d8d58da9628bcbdcf2aa5b35e319419871c609
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-08-25 21:50:52 +0300

    ass

--- scripts/main.js
+++ scripts/main.js
@@ -178,14 +178,14 @@
                  row1.x = a;
                  row1.y = y + b;
                  app.stage.addChild(row1);
-                 y += row1.height + 5;
+                 y += row1.height + 1;
              }
              if (i > 4) {
                  let row2 = createFriendBox(data.response.items[i].id, data.response.items[i].first_name, data.response.items[i].photo_50);
                  row2.x = a + 246;
                  row2.y = y1 + b;
                  app.stage.addChild(row2);
-                 y1 += row2.height + 5;
+                 y1 += row2.height + 1;
              }
          }
      })
