d9f1f5fb6f303813144ec257e9406d6fade45ecc
Dmitriy <chuprin-dmitriy@mail.ru>
2019-08-26 17:05:31 +0300

    add

--- scripts/main.js
+++ scripts/main.js
@@ -186,7 +186,7 @@
                     }                    
                 });
                  y += row1.height + 10;
-             }}
+             }
              if (i > 3) {
                  let row2 = createFriendBox(data.response.items[i].id, data.response.items[i].first_name, data.response.items[i].photo_50);
                  row2.x = a + 246;
