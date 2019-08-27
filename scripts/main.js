24c89a7b56d861742f146d97cdf36855e77a87ff
Dmitriy <chuprin-dmitriy@mail.ru>
2019-08-27 15:54:00 +0300

    add

--- scripts/main.js
+++ scripts/main.js
@@ -142,7 +142,7 @@
      app.stage.addChild(sendAll);
      listFriends();
 
-     let t = new Tink(PIXI, renderer.view);
+     let t = new Tink(PIXI, app.renderer.view);
      let pointer = t.makePointer();
      pointer.tap = () => console.log("Press");
      pointer.hitTestSprite(sendAll);
