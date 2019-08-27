de8bb5d06c166685807ebf37f68bfcbb315b6745
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-08-27 23:47:38 +0300

    add

--- scripts/main.js
+++ scripts/main.js
@@ -189,7 +189,7 @@
              let row1 = createFriendBox(data.response.items[i].first_name, data.response.items[i].photo_50);
              row1.interactive = true;
              row1.buttonMode = true;
-             row1.on('pointdown', sendMessage);
+             row1.on('click', sendMessage);
              if (i < 4) {
                  row1.x = a;
                  row1.y = y + b;
